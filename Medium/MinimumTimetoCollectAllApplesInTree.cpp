9d1a86ed7e9f8e0bfa410c46ea8977333c4426ce
muskan.mangla <muskanmangla70@gmail.com>
2020-05-13 09:25:42 +0530

    complication solved

--- Medium/MinimumTimetoCollectAllApplesInTree.cpp
+++ Medium/MinimumTimetoCollectAllApplesInTree.cpp
@@ -51,3 +51,19 @@ public:
         return g.dfs(0,vis,hasApple).second;
     }
 };
+
+//Minimum Time to Collect All Apples in a Tree : https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+class Solution {
+public:
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        int ans=0;
+        for(int i=edges.size()-1;i>=0;i--){
+            if(hasApple[edges[i][1]]){
+                ans++;
+                hasApple[edges[i][0]]=true;
+            }
+        }
+        return ans*2;
+    }
+};
+//This approach is only valid if edges are given from root to leaves order.
